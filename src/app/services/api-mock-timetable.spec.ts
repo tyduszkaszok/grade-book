e52ef0ec66063cd427519091935529ad07a3d20c
tyduszkaszok <bobspychacz@gmail.com>
2025-12-13 19:30:16 +0100

    Add: personalised timetable and grade book

--- src/app/services/api-mock-timetable.spec.ts
+++ src/app/services/api-mock-timetable.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ApiMockTimetable } from './api-mock-timetable';
+import { ApiMockTimetable } from './api-mock';
 
 describe('ApiMockTimetable', () => {
   let service: ApiMockTimetable;
--- src/app/services/mock-db.ts
+++ src/app/services/mock-db.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
-import { ApiStudents } from './api-students';
+
 
 @Injectable({
   providedIn: 'root',
 })
-export class MockTimetable implements InMemoryDbService {
+export class MockDb implements InMemoryDbService {
   createDb() {
     const schedules = [
       // Poniedziałek - 4b
@@ -79,6 +79,59 @@ export class MockTimetable implements InMemoryDbService {
       { id: 50, class: "4c", day: "Piątek", subject: "Godzina wychowawcza", hour: "12:00" }
     ];
     
-    return { schedules };
+    const students = [
+      // student_1 - Oceny
+      { id: 1, username: "student_1", subject: "Matematyka", grade: 5 },
+      { id: 2, username: "student_1", subject: "Matematyka", grade: 4 },
+      { id: 3, username: "student_1", subject: "Matematyka", grade: 6 },
+      { id: 4, username: "student_1", subject: "Język polski", grade: 4 },
+      { id: 5, username: "student_1", subject: "Język polski", grade: 5 },
+      { id: 6, username: "student_1", subject: "Język angielski", grade: 5 },
+      { id: 7, username: "student_1", subject: "Język angielski", grade: 5 },
+      { id: 8, username: "student_1", subject: "Język angielski", grade: 6 },
+      { id: 9, username: "student_1", subject: "Fizyka", grade: 6 },
+      { id: 10, username: "student_1", subject: "Fizyka", grade: 5 },
+      { id: 11, username: "student_1", subject: "Fizyka", grade: 5 },
+      { id: 12, username: "student_1", subject: "Chemia", grade: 4 },
+      { id: 13, username: "student_1", subject: "Chemia", grade: 5 },
+      { id: 14, username: "student_1", subject: "Biologia", grade: 5 },
+      { id: 15, username: "student_1", subject: "Biologia", grade: 4 },
+      { id: 16, username: "student_1", subject: "Historia", grade: 5 },
+      { id: 17, username: "student_1", subject: "Historia", grade: 5 },
+      { id: 18, username: "student_1", subject: "Geografia", grade: 4 },
+      { id: 19, username: "student_1", subject: "Geografia", grade: 5 },
+      { id: 20, username: "student_1", subject: "Informatyka", grade: 6 },
+      { id: 21, username: "student_1", subject: "Informatyka", grade: 6 },
+      { id: 22, username: "student_1", subject: "Wychowanie fizyczne", grade: 5 },
+      { id: 23, username: "student_1", subject: "Wychowanie fizyczne", grade: 6 },
+      { id: 24, username: "student_1", subject: "Wiedza o społeczeństwie", grade: 5 },
+      
+      // student_2 - Oceny
+      { id: 25, username: "student_2", subject: "Matematyka", grade: 3 },
+      { id: 26, username: "student_2", subject: "Matematyka", grade: 4 },
+      { id: 27, username: "student_2", subject: "Matematyka", grade: 4 },
+      { id: 28, username: "student_2", subject: "Język polski", grade: 5 },
+      { id: 29, username: "student_2", subject: "Język polski", grade: 4 },
+      { id: 30, username: "student_2", subject: "Język angielski", grade: 4 },
+      { id: 31, username: "student_2", subject: "Język angielski", grade: 5 },
+      { id: 32, username: "student_2", subject: "Język angielski", grade: 4 },
+      { id: 33, username: "student_2", subject: "Fizyka", grade: 4 },
+      { id: 34, username: "student_2", subject: "Fizyka", grade: 3 },
+      { id: 35, username: "student_2", subject: "Fizyka", grade: 4 },
+      { id: 36, username: "student_2", subject: "Chemia", grade: 5 },
+      { id: 37, username: "student_2", subject: "Chemia", grade: 5 },
+      { id: 38, username: "student_2", subject: "Biologia", grade: 4 },
+      { id: 39, username: "student_2", subject: "Biologia", grade: 5 },
+      { id: 40, username: "student_2", subject: "Historia", grade: 4 },
+      { id: 41, username: "student_2", subject: "Historia", grade: 4 },
+      { id: 42, username: "student_2", subject: "Geografia", grade: 5 },
+      { id: 43, username: "student_2", subject: "Geografia", grade: 4 },
+      { id: 44, username: "student_2", subject: "Informatyka", grade: 5 },
+      { id: 45, username: "student_2", subject: "Informatyka", grade: 5 },
+      { id: 46, username: "student_2", subject: "Wychowanie fizyczne", grade: 6 },
+      { id: 47, username: "student_2", subject: "Wychowanie fizyczne", grade: 5 },
+      { id: 48, username: "student_2", subject: "Wiedza o społeczeństwie", grade: 4 }
+    ];
+    return { schedules, students };
   }
 }
